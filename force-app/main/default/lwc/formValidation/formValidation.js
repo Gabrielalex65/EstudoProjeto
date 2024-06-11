3af7fb292c5651fb0403a50e4f891727244fa1c5
Gabriel <gabriel.alex@globant.com>
2024-06-11 17:41:00 -0300

    JS com tratativa

--- force-app/main/default/lwc/formValidation/formValidation.js
+++ force-app/main/default/lwc/formValidation/formValidation.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class FormValidation extends LightningElement {}
+export default class formValidation extends LightningElement {
+    handleSuccess(event) {
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: event.detail.apiName + ' created.',
+                variant: 'success',
+            })
+        );
+    }
+
+    handleChange(event) {
+        console.log('You selected an account: ' + event.detail.value[0]);
+    }
+}
